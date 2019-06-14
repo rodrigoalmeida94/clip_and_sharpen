2ef52e7f8e15dae660026a6154fd6560be1b16dd
Rodrigo Almeida <rodrigo.almeida94@outlook.pt>
2019-06-14 21:37:04 +0200

    Add input and output tmp folder

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ This is a sample processing block that clips the input image to a given extent a
 """
 import json
 import shutil
+import os
 import rasterio
 from rasterio.windows import Window
 import numpy as np
@@ -16,16 +17,19 @@ def load_input():
     """
     Load the input image from the filesystem
     """
-    return rasterio.open(IMGFILE)
+    input_file = os.listdir('/tmp/input')
+    if len(input_file) > 1:
+        raise(ValueError, "More than 1 input file.")
+    return '/tmp/input/'+input_file[0]
 
-def clip_input():
+def clip_input(input_path, output_path='cropped.tif'):
     """
-    Clip 200 by 200 chip of image
+    Clip a chip of image
     :img: rasterio dataset
     """
 
-    with rasterio.open(IMGFILE, 'r') as img:
-        window = Window(1000, 1000, 1000, 1000)
+    with rasterio.open(input_path, 'r') as img:
+        window = Window(1000, 1000, 250, 250)
         kwargs = img.meta.copy()
         kwargs.update({
         'driver':'GTiff',
@@ -33,9 +37,10 @@ def clip_input():
             'width': window.width,
             'transform': img.window_transform(window)})
         print(kwargs)
-        with rasterio.open('tmp/cropped.tif', 'w', **kwargs) as out:
+        with rasterio.open(output_path, 'w', **kwargs) as out:
                 out.write(img.read(window=window,
                                    out_shape=(img.count, window.height, window.width)))
+        return output_path
 
 def high_pass_filter(data):
     blurred = ndimage.gaussian_filter(data, 3)
@@ -43,31 +48,32 @@ def high_pass_filter(data):
     sharpened = data + (data - filter_blurred)
     return sharpened
 
-def run_high_pass():
-    with rasterio.open('tmp/cropped.tif') as cropped:
+def run_high_pass(input_path, output_path='high_pass.tif'):
+    with rasterio.open(input_path) as cropped:
         kwargs = cropped.meta.copy()
-        with rasterio.open('tmp/high_pass.tif', 'w', **kwargs) as out:
+        with rasterio.open(output_path, 'w', **kwargs) as out:
             out.write(high_pass_filter(cropped.read(1)), 1)
             out.write(high_pass_filter(cropped.read(2)), 2)
             out.write(high_pass_filter(cropped.read(3)), 3)
             out.write(high_pass_filter(cropped.read(4)), 4)
+    return output_path
 
-def run():
-    clip_input()
-    run_high_pass()
+def run(data):
+    output_path = run_high_pass(clip_input(data))
+    return output_path
 
 
-def write_output(result):
+def write_output(result_path, output_file_name):
     """
     Write the result data to the /tmp/output directory.
 
     If you are storing image data, you would need to then copy that data into this directory as well.
     """
+    shutil.copy(result_path, "/tmp/output/"+output_file_name)
 
 
 
 if __name__ == "__main__":
-    #data = load_input()
-    #result = run(data)
-    #write_output(result)
-    run()
+    data = load_input()
+    result = run(data)
+    write_output(result, "sharpened_"+os.path.basename(data))
