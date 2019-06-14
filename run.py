edfd3e9484261c621b9ff4865975829580fe2ea6
Rodrigo Almeida <rodrigo.almeida94@outlook.pt>
2019-06-14 22:12:56 +0200

    Add logging messages

--- run.py
+++ run.py
@@ -3,11 +3,14 @@ This is a sample processing block that clips the input image to a given extent a
 """
 import json
 import shutil
+import time
 import os
 import rasterio
 from rasterio.windows import Window
 import numpy as np
 from scipy import ndimage
+import logging
+logging.basicConfig(level=logging.INFO)
 
 def load_input():
     """
@@ -17,6 +20,7 @@ def load_input():
     input_file = os.listdir('/tmp/input')
     if len(input_file) > 1:
         raise(ValueError, "More than 1 input file.")
+    logging.info('Loading %s' % input_file[0])
     return '/tmp/input/'+input_file[0]
 
 def clip_input(input_path, output_path='cropped.tif'):
@@ -27,6 +31,7 @@ def clip_input(input_path, output_path='cropped.tif'):
     """
 
     with rasterio.open(input_path, 'r') as img:
+        logging.info('Clipping %s' % input_path)
         window = Window(1000, 1000, 250, 250)
         kwargs = img.meta.copy()
         kwargs.update({
@@ -37,6 +42,7 @@ def clip_input(input_path, output_path='cropped.tif'):
         with rasterio.open(output_path, 'w', **kwargs) as out:
                 out.write(img.read(window=window,
                                    out_shape=(img.count, window.height, window.width)))
+        logging.info('Writting clipped image to %s' % input_path)
         return output_path
 
 def high_pass_filter(data):
@@ -58,12 +64,14 @@ def run_high_pass(input_path, output_path='high_pass.tif'):
     """
 
     with rasterio.open(input_path) as cropped:
+        logging.info('Applying sharpening to %s' % input_path)
         kwargs = cropped.meta.copy()
         with rasterio.open(output_path, 'w', **kwargs) as out:
             out.write(high_pass_filter(cropped.read(1)), 1)
             out.write(high_pass_filter(cropped.read(2)), 2)
             out.write(high_pass_filter(cropped.read(3)), 3)
             out.write(high_pass_filter(cropped.read(4)), 4)
+    logging.info('Writting sharpened image to %s' % output_path)
     return output_path
 
 def run(data):
@@ -80,9 +88,13 @@ def write_output(result_path, output_file_name):
     :param str result_path: Path of input image
     :param str output_file_name: File name to write to output folder
     """
+    logging.info('Copying sharpened image to %s' % "/tmp/output/"+output_file_name)
     shutil.copy(result_path, "/tmp/output/"+output_file_name)
 
 if __name__ == "__main__":
+    start = time.time()
     data = load_input()
     result = run(data)
     write_output(result, "sharpened_"+os.path.basename(data))
+    end = time.time()
+    logging.info("Clip and Sharpen ran in %s s" % round(end - start, 2))
