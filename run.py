6361671258e2e16226315bb96c4ef93b767e0684
Rodrigo Almeida <rodrigo.almeida94@outlook.pt>
2019-06-15 04:08:53 +0200

    Beautify

--- run.py
+++ run.py
@@ -20,6 +20,7 @@ parser.add_argument('--alpha', default=15, type=int,
                     help='Parameter for edge salience in sharpen method. Set to 0 to return source imagery.')
 args = parser.parse_args()
 
+
 def load_input():
     """
     Load the input image from the filesystem
@@ -29,7 +30,8 @@ def load_input():
     if len(input_file) > 1:
         raise ValueError("More than 1 input file.")
     logging.info('Loading %s' % input_file[0])
-    return '/tmp/input/'+input_file[0]
+    return '/tmp/input/' + input_file[0]
+
 
 def clip_input(input_path, clip_coords=(1000, 1000, 250, 250), output_path='cropped.tif'):
     """
@@ -40,22 +42,26 @@ def clip_input(input_path, clip_coords=(1000, 1000, 250, 250), output_path='crop
     """
 
     with rasterio.open(input_path, 'r') as img:
-        logging.info('Clipping %s with coordinates (%d %d %d %d)' % (input_path, *clip_coords))
-        logging.info('%s is h %d by w %d' % (input_path, img.height, img.width))
+        logging.info('Clipping %s with coordinates (%d %d %d %d)' %
+                     (input_path, *clip_coords))
+        logging.info('%s is h %d by w %d' %
+                     (input_path, img.height, img.width))
         window = Window(*clip_coords)
         if (window.row_off + window.height) > img.height or (window.col_off + window.width) > img.width:
             raise ValueError('Clip coordinates are out of image bounds.')
         kwargs = img.meta.copy()
         kwargs.update({
-        'driver':'GTiff',
+            'driver': 'GTiff',
             'height': window.height,
             'width': window.width,
             'transform': img.window_transform(window)})
         with rasterio.open(output_path, 'w', **kwargs) as out:
-                out.write(img.read(window=window,
-                                   out_shape=(img.count, window.height, window.width)))
-        logging.info('Writting clipped image to %s, with h %d by w %d' % (input_path, window.height, window.width))
+            out.write(img.read(window=window,
+                               out_shape=(img.count, window.height, window.width)))
+        logging.info('Writting clipped image to %s, with h %d by w %d' %
+                     (input_path, window.height, window.width))
         return output_path
+
 
 def high_pass_filter(data, alpha=15):
     """
@@ -67,8 +73,9 @@ def high_pass_filter(data, alpha=15):
     blurred = ndimage.gaussian_filter(data, 3)
     filter_blurred = ndimage.gaussian_filter(blurred, 1)
     noise = (blurred - filter_blurred)
-    sharpened = data - alpha*noise
+    sharpened = data - alpha * noise
     return sharpened
+
 
 def run_high_pass(input_path, output_path='high_pass.tif'):
     """
@@ -78,13 +85,16 @@ def run_high_pass(input_path, output_path='high_pass.tif'):
     """
 
     with rasterio.open(input_path) as cropped:
-        logging.info('Applying sharpening to %s with alpha %d' % (input_path, args.alpha))
+        logging.info('Applying sharpening to %s with alpha %d' %
+                     (input_path, args.alpha))
         kwargs = cropped.meta.copy()
         with rasterio.open(output_path, 'w', **kwargs) as out:
-            for band in range(1, cropped.count+1):
-                out.write(high_pass_filter(cropped.read(band), alpha=args.alpha), band)
+            for band in range(1, cropped.count + 1):
+                out.write(high_pass_filter(
+                    cropped.read(band), alpha=args.alpha), band)
     logging.info('Writting sharpened image to %s' % output_path)
     return output_path
+
 
 def run(data):
     """
@@ -95,6 +105,7 @@ def run(data):
     output_path = run_high_pass(clip_input(data, args.clip_coords))
     return output_path
 
+
 def write_output(result_path, input_file_name):
     """
     Write the result to tmp/output with a given name
@@ -102,9 +113,12 @@ def write_output(result_path, input_file_name):
     :param str input_file_name: File name of original image
     """
 
-    output_file_name = "sharpened_"+os.path.splitext(os.path.basename(input_file_name))[0]+".tif"
-    logging.info('Copying sharpened image to %s' % "/tmp/output/"+output_file_name)
-    shutil.copy(result_path, "/tmp/output/"+output_file_name)
+    output_file_name = "sharpened_" + \
+        os.path.splitext(os.path.basename(input_file_name))[0] + ".tif"
+    logging.info('Copying sharpened image to %s' %
+                 "/tmp/output/" + output_file_name)
+    shutil.copy(result_path, "/tmp/output/" + output_file_name)
+
 
 if __name__ == "__main__":
     start = time.time()
