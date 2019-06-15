eb28f8a1d6b8d8d0a4edef78a47059fd71f2794c
Rodrigo Almeida <rodrigo.almeida94@outlook.pt>
2019-06-15 13:22:57 +0200

    Fix numpy array equality test

--- test.py
+++ test.py
@@ -10,14 +10,17 @@ from rasterio.windows import Window
 import numpy as np
 from scipy import ndimage
 import unittest
-from run import *
+from run import high_pass_filter
+
 
 class TestClipSharpen(unittest.TestCase):
 
     def test_high_pass_filter(self):
-        test_array = np.zeros((3,3))
+        test_array = np.zeros((3, 3))
         self.assertEqual(high_pass_filter(test_array).size, 9)
-        self.assertEqual(high_pass_filter(test_array), test_array)
+        self.assertEqual(high_pass_filter(
+            test_array).tolist(), test_array.tolist())
+
 
 if __name__ == '__main__':
     unittest.main()
